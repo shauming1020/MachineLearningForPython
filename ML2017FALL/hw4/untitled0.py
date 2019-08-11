cd7b2cdb4ebb2bd9861eed4f4cc6a5cda0a03a86
shauming1020 <50039782+shauming1020@users.noreply.github.com>
2019-08-11 12:00:14 +0800

    Add Stacked Ensemble tech.

--- ML2017FALL/hw4/untitled0.py
+++ ML2017FALL/hw4/untitled0.py
@@ -73,7 +73,7 @@ def Build_model(SEQUENCES_SIZE, LEXION_SIZE, HIDDEN_DIM):
     from keras.models import Model
     ### Parameters ###
     RETURN_SEQUENCE = False
-    DROPOUT_RATE = 0.4    
+    DROPOUT_RATE = 0.4
     ##################
     # Input
     inputs = Input(shape=(SEQUENCES_SIZE,),dtype='int32')
@@ -99,8 +99,8 @@ def Build_model(SEQUENCES_SIZE, LEXION_SIZE, HIDDEN_DIM):
 def Train_model(X,Y,X_val,Y_val,model,now):
     from keras.callbacks import ModelCheckpoint, EarlyStopping
     ### Parameter ###
-    PATIENCE = 5
-    EPOCHS = 30
+    PATIENCE = 8
+    EPOCHS = 32
     BATCH_SIZE = 5120
     #################
     cp = ModelCheckpoint(MODE_PATH + '/model_'+ str(now) +'.h5',
@@ -113,7 +113,34 @@ def Train_model(X,Y,X_val,Y_val,model,now):
                         callbacks=[cp,es])
     return history
 
-def ensemble(NUM):
+def Stacked_dataset(members, X_test):
+    # Prepare a training dataset for the meta-learner
+    stackX = None
+    for model in members:
+        y_hat = model.predict(X_test,verbose=0)
+        # stack predictions into [rows,members,probabilities]
+        if stackX is None:
+            stackX = y_hat
+        else:
+            stackX = np.dstack((stackX,y_hat))
+    # flatten predictions to [rows,members,probabilities]
+    stackX = stackX.reshape((stackX.shape[0], stackX.shape[1]*stackX.shape[2]))
+    return stackX
+
+def Fit_stacked_model(members,X_test,y_test):
+    stackedX = Stacked_dataset(members,X_test)
+    from sklearn.linear_model import LogisticRegression
+    model = LogisticRegression()
+    y_test = np.argmax(y_test,axis=-1) # LogisticRegression's target values
+    model.fit(stackedX, y_test)
+    return model
+
+def Stacked_prediction(members,model,ob):
+    stackedX = Stacked_dataset(members,ob)
+    y_hat = model.predict(stackedX)
+    return y_hat
+    
+def Stacked_Ensemble(NUM):
     ## Read Raw Data
     with open(RAWDATA_LABEL_PATH, encoding = 'utf8') as f:
         raw_label = f.readlines()
@@ -123,49 +150,53 @@ def ensemble(NUM):
     from keras.utils import to_categorical    
     X_raw, y_raw, word_index = Preprocessing(X_raw,y_raw,'sequences')
     y_raw = to_categorical(y_raw).astype('int32')
+    ## Get Training set and Testing set
+    from sklearn.model_selection import train_test_split   
+    X_train,X_test,y_train,y_test = train_test_split(X_raw,y_raw,train_size=0.7,shuffle=True)    
     ### Training Parameters ###
-    TRAIN_SIZE = 0.7
+    VAL_SIZE = 0.2
     SEQUENCES_SIZE = np.shape(X_raw)[1] # columns dim size of sequences
     LEXION_SIZE = len(word_index)
     HIDDEN_DIM = 32
     ###########################
-    ## Ensemble Training
-    from sklearn.model_selection import train_test_split    
+    ## Train and Save Sub-Models
     for i in range(NUM):
         # Get Training set and Validation set
-        X_train,X_val,y_train,y_val = train_test_split(X_raw,y_raw,train_size=TRAIN_SIZE,
-                                               random_state=i,
-                                               shuffle=True)
+        X,X_val,y,y_val = train_test_split(X_train,y_train,
+                                           test_size=VAL_SIZE,
+                                           random_state=i,
+                                           shuffle=True)
         # Build new model
         model = Build_model(SEQUENCES_SIZE,LEXION_SIZE,HIDDEN_DIM)
         # Train the model
-        Train_model(X_train,y_train,X_val,y_val,model,i)
+        Train_model(X,y,X_val,y_val,model,i)
         
+    ## Separate Stacking Model
+    from keras.models import load_model
+    all_models = list()
+    for i in range(NUM):
+        model = load_model(MODE_PATH + '/model_'+ str(i) +'.h5')
+        all_models.append(model)   
+    model = Fit_stacked_model(all_models,X_test,y_test)
+    ## Evaluate The Model
+    from sklearn.metrics import accuracy_score
+    yhat = Stacked_prediction(all_models, model, X_test)
+    acc = accuracy_score(y_test, yhat)
+    print('Stacked Test Accuracy: %.3f' % acc)
+    
     ## Read Observe Data
     with open(OBSERVE_PATH, encoding = 'utf8') as f:
         ob = f.readlines()
     ob_id = [seg.strip().split(",",1)[0] for seg in ob][1:]
     ob = [seg.strip().split(",",1)[1] for seg in ob][1:]
     ## Preprocessing
-    ob, ob_id, word_index = Preprocessing(ob, ob_id, 'sequences',SEQUENCES_SIZE)
-        
-    ### Predicting Parameters ###
-    BATCH_SIZE = 5120
-    #############################
-    ## Ensemble Predicting
-    pred = np.zeros((len(ob_id),2))
-    from keras.models import load_model
-    for i in range(NUM):
-        model = load_model(MODE_PATH + '/model_'+ str(i) +'.h5')
-        pred += model.predict(ob,batch_size=BATCH_SIZE,verbose=1)
-    pred /= NUM
-    
+    ob, ob_id, word_index = Preprocessing(ob, ob_id, 'sequences', SEQUENCES_SIZE)   
     ## Submission
     print("Submission ...")
+    pred = Stacked_prediction(all_models,model,ob)
     sampleSubmission = pd.read_csv(SUBMISSION_PATH)
-#    sampleSubmission["label"] = np.round(pred)
     sampleSubmission["label"] = np.argmax(pred,axis=-1)
     sampleSubmission.to_csv(SUBMISSION_PATH,index=None)   
-    
+
 if __name__ == '__main__':
-    ensemble(5)
+    Stacked_Ensemble(5)
