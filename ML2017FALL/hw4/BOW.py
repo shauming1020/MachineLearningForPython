15b679e01a3264fcfddb47a9d30bb5c048c9b178
shauming1020 <50039782+shauming1020@users.noreply.github.com>
2019-08-16 22:42:43 +0800

    Add files via upload

--- ML2017FALL/hw4/BOW.py
+++ ML2017FALL/hw4/BOW.py
@@ -36,26 +36,27 @@ def Preprocess(string, use_stem = True):
             string = string.replace(punct, punct[0])
     return stemmer.stem_sentence(string)
 
-def Encoding(corpus,corpus_y,pad_length=None):
-    from keras.preprocessing.text import Tokenizer
-    from keras.preprocessing.sequence import pad_sequences
-    ### Parameters ###
-    VOCAB_SIZE = 20000 # the maximum number of words to keep, based on word frequency.
-    BOW_MODE = 'count' # default  
-    ## Data preprocessing (tokenize, padding)
+def Create_vocab(corpus,VOCAB_SIZE,save=False):
+    from keras.preprocessing.text import Tokenizer    
+     ## Data preprocessing (tokenize, padding)
+#    VOCAB_SIZE  = 1000 # the maximum number of words to keep, based on word frequency.
     tokenizer = Tokenizer(num_words=VOCAB_SIZE, 
-                          filters='!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n', 
-                          lower=True, 
-                          split=' ', 
-                          char_level=False, 
-                          oov_token=None, 
-                          document_count=0)
+                          filters='!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n')
     tokenizer.fit_on_texts(corpus)
-    word_index = tokenizer.word_index 
+    word_index = tokenizer.word_index
+    if save:
+        import pickle
+        with open('tokenizer.pickle','wb') as f:
+            pickle.dump(tokenizer, f, protocol=pickle.HIGHEST_PROTOCOL)
+    return tokenizer, word_index
+    
+def Encoding(corpus,tokenizer,pad_length=None):
+    from keras.preprocessing.sequence import pad_sequences
+    BOW_MODE = 'count' # default  
     ## Data preprocessing (Bag Of Word)
     # To avoid the overloading of dimension, it could decress the dict size.
     bow = tokenizer.texts_to_matrix(corpus,BOW_MODE).astype('int32')
-    return bow, corpus_y, word_index
+    return bow
 
 def Build_model(SEQUENCES_SIZE):
     ## Build Simple RNN Model
@@ -82,8 +83,8 @@ def Build_model(SEQUENCES_SIZE):
 def Train_model(X,Y,X_val,Y_val,model,now):
     from keras.callbacks import ModelCheckpoint, EarlyStopping
     ### Parameter ###
-    PATIENCE = 1
-    EPOCHS = 1
+    PATIENCE = 8
+    EPOCHS = 24
     BATCH_SIZE = 5120
     #################
     cp = ModelCheckpoint(MODE_PATH + '/model_' + str(now) + '.h5',
@@ -101,22 +102,26 @@ def Stacked_dataset(members, X_test):
     stackX = None
     for model in members:
         y_hat = model.predict(X_test,verbose=0)
+        y_hat = np.argmax(y_hat,axis=-1)
         # stack predictions into [rows,members,probabilities]
         if stackX is None:
             stackX = y_hat
         else:
-            stackX = np.dstack((stackX,y_hat))
+            stackX = np.vstack((stackX,y_hat)) 
     # flatten predictions to [rows,members,probabilities]
-    if len(members) == 1:
-            return stackX
-    else:
-        return stackX.reshape((stackX.shape[0], stackX.shape[1]*stackX.shape[2]))
+    return stackX.transpose()
 
-def Fit_stacked_model(members,X_test,y_test):
+def Fit_stacked_model(members,X_test,yc_test):
     stackedX = Stacked_dataset(members,X_test)
+    from sklearn.ensemble import VotingClassifier, RandomForestClassifier
     from sklearn.linear_model import LogisticRegression
-    model = LogisticRegression()
-    model.fit(stackedX, y_test)
+    from sklearn.naive_bayes import GaussianNB
+    clf1 = LogisticRegression(solver='lbfgs',multi_class='multinomial',
+                              random_state=1)
+#    clf2 = RandomForestClassifier(n_estimators=50, random_state=1)
+#    clf3 = GaussianNB()
+    model = VotingClassifier(estimators=[('rg',clf1)])
+    model.fit(stackedX, yc_test)
     return model
 
 def Stacked_prediction(members,model,ob):
@@ -133,12 +138,14 @@ def Stacked_Ensemble(NUM):
     ## Preprocessing
     from keras.utils import to_categorical  
     X_raw_seq = [Preprocess(seg) for seg in X_raw]
-    y_raw_cat = to_categorical(y_raw).astype('int32') 
+    y_raw = to_categorical(y_raw).astype('int32') 
+    ## Create the Vocab
+    tokenizer, word_index = Create_vocab(X_raw_seq,5000,save=True)
     ## Encoding  
-    X_raw, y_raw, word_index = Encoding(X_raw_seq,y_raw_cat)   
+    X_raw = Encoding(X_raw_seq,tokenizer)   
     ## Get Training set and Testing set
     from sklearn.model_selection import train_test_split   
-    X_train,X_test,y_train,y_test = train_test_split(X_raw,y_raw,train_size=0.7,shuffle=True)    
+    X_train,X_test,y_train,y_test = train_test_split(X_raw,y_raw,train_size=0.7,shuffle=False)    
     ### Training Parameters ###
     VAL_SIZE = 0.2
     SEQUENCES_SIZE = np.shape(X_raw)[1] # columns dim size of sequences
@@ -178,8 +185,12 @@ def Stacked_Ensemble(NUM):
     ## Preprocessing
     ob_id = np.asarray(ob_id,dtype='int32')
     ob = [Preprocess(seg) for seg in ob]  
+    ## Read the Vocab
+    import pickle
+    with open('tokenizer_vocab.pickle', 'rb') as f:
+        tokenizer = pickle.load(f)
     ## Encoding
-    ob, ob_id, word_index = Encoding(ob, ob_id) 
+    ob = Encoding(ob, tokenizer) 
     ## Submission
     print("Submission ...")
     pred = Stacked_prediction(all_models,model,ob)
@@ -188,4 +199,4 @@ def Stacked_Ensemble(NUM):
     sampleSubmission.to_csv(SUBMISSION_PATH,index=None)   
 
 if __name__ == '__main__':
-    Stacked_Ensemble(1) 
+    Stacked_Ensemble(5) 
--- ML2017FALL/hw4/RNN.py
+++ ML2017FALL/hw4/RNN.py
@@ -35,20 +35,26 @@ def Preprocess(string, use_stem = True):
             string = string.replace(punct, punct[0])
     return stemmer.stem_sentence(string)
 
-def Encoding(corpus,corpus_y,pad_length=None):
+def Create_vocab(corpus,save=False):
     from keras.preprocessing.text import Tokenizer
-    from keras.preprocessing.sequence import pad_sequences
     ### Parameters ###
     VOCAB_SIZE = None # the maximum number of words to keep, based on word frequency.
     ## Data preprocessing (tokenize, padding)
     tokenizer = Tokenizer(num_words=VOCAB_SIZE, 
                           filters='!"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n')
     tokenizer.fit_on_texts(corpus)
-    word_index = tokenizer.word_index 
+    word_index = tokenizer.word_index
+    if save:
+        import pickle
+        with open('tokenizer.pickle','wb') as f:
+            pickle.dump(tokenizer, f, protocol=pickle.HIGHEST_PROTOCOL)
+    return tokenizer, word_index
+
+def Encoding(corpus,tokenizer,pad_length=None):
+    from keras.preprocessing.sequence import pad_sequences
     sequences = tokenizer.texts_to_sequences(corpus)
     sequences = pad_sequences(sequences, maxlen=pad_length) 
-    return sequences, corpus_y, word_index
-
+    return sequences
 
 def PreTrain_weights(sequences, word_index, HIDDEN_DIM):
     from gensim.models import Word2Vec
@@ -70,22 +76,17 @@ def PreTrain_weights(sequences, word_index, HIDDEN_DIM):
             print(word," not in w2v model")
     return SEQUENCES_SIZE, EMB_INPUT_SIZE, embedding_matrix
 
-
 def Build_model(SEQUENCES_SIZE, EMB_INPUT_SIZE, HIDDEN_DIM, embedding_matrix=[]):
     ## Build Simple RNN Model
     from keras.layers import Input,InputLayer,Embedding,BatchNormalization, Dense, Bidirectional,LSTM,Dropout,GRU,Activation
     from keras.models import Model
     from keras import regularizers
-    ### Parameters ###
-
-    ##################
     # Input
     inputs = Input(shape=(SEQUENCES_SIZE,),dtype='int32')  
     # Embedding layer
     # This the important issue that input_dim: int > 0. 
     # Size of the vocabulary, i.e. maximum integer index + 1.    
     if embedding_matrix != []:
-
         embedding_inputs = Embedding(input_dim=EMB_INPUT_SIZE,
                                      output_dim=HIDDEN_DIM,
                                      weights=[embedding_matrix],
@@ -95,13 +96,28 @@ def Build_model(SEQUENCES_SIZE, EMB_INPUT_SIZE, HIDDEN_DIM, embedding_matrix=[])
                                      output_dim=HIDDEN_DIM,
                                      trainable=True)(inputs)
     # LSTM
-    RNN_cell = LSTM(HIDDEN_DIM,return_sequences=False,dropout=0.4)
-    RNN_output = RNN_cell(embedding_inputs)
+    lstm1 = Bidirectional(LSTM(HIDDEN_DIM,return_sequences=True,dropout=0.3,
+                               kernel_initializer='he_uniform'))
+#    lstm1 = LSTM(HIDDEN_DIM,return_sequences=True,dropout=0.4,
+#                               kernel_initializer='he_uniform')
+    lstm1_output = lstm1(embedding_inputs)
+    lstm2 = Bidirectional(LSTM(64,return_sequences=True,dropout=0.2,
+                               kernel_initializer='he_uniform'))
+#    lstm2 = LSTM(64,return_sequences=False,dropout=0.4,
+#                               kernel_initializer='he_uniform')
+    lstm2_output = lstm2(lstm1_output)   
+    lstm3 = Bidirectional(LSTM(32,return_sequences=False,dropout=0.1,
+                               kernel_initializer='he_uniform'))
+#    lstm2 = LSTM(64,return_sequences=False,dropout=0.4,
+#                               kernel_initializer='he_uniform')
+    lstm3_output = lstm3(lstm2_output)      
     
+    lstm3_batch = BatchNormalization()(lstm3_output)
     # DNN (classify)
+    d1 = Dropout(0.5)(Dense(64,activation='sigmoid')(lstm3_batch))
+    d2 = Dropout(0.5)(Dense(32,activation='sigmoid')(d1))
     outputs = Dense(2,activation='softmax',
-                    kernel_regularizer=regularizers.l2(0.01))(RNN_output)
-    
+                    kernel_regularizer=regularizers.l2(0.001))(d2)
     model = Model(inputs,outputs)
     model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['acc'])
     model.summary()
@@ -110,9 +126,9 @@ def Build_model(SEQUENCES_SIZE, EMB_INPUT_SIZE, HIDDEN_DIM, embedding_matrix=[])
 def Train_model(X,Y,X_val,Y_val,model,now):
     from keras.callbacks import ModelCheckpoint, EarlyStopping
     ### Parameter ###
-    PATIENCE = 1
-    EPOCHS = 1
-    BATCH_SIZE = 5120
+    PATIENCE = 64
+    EPOCHS = 1280
+    BATCH_SIZE = 2560
     #################
     cp = ModelCheckpoint(MODE_PATH + '/model_' + str(now) + '.h5',
                          verbose=1,save_best_only=True,monitor='val_acc')
@@ -129,22 +145,25 @@ def Stacked_dataset(members, X_test):
     stackX = None
     for model in members:
         y_hat = model.predict(X_test,verbose=0)
+        y_hat = np.argmax(y_hat,axis=-1)
         # stack predictions into [rows,members,probabilities]
         if stackX is None:
             stackX = y_hat
         else:
-            stackX = np.dstack((stackX,y_hat))
-    # flatten predictions to [rows,members,probabilities]
-    if len(members) == 1:
-            return stackX
-    else:
-        return stackX.reshape((stackX.shape[0], stackX.shape[1]*stackX.shape[2]))
+            stackX = np.vstack((stackX,y_hat)) 
+    return stackX.transpose()
 
-def Fit_stacked_model(members,X_test,y_test):
+def Fit_stacked_model(members,X_test,yc_test):
     stackedX = Stacked_dataset(members,X_test)
+    from sklearn.ensemble import VotingClassifier, RandomForestClassifier
     from sklearn.linear_model import LogisticRegression
-    model = LogisticRegression()
-    model.fit(stackedX, y_test)
+    from sklearn.naive_bayes import GaussianNB
+    clf1 = LogisticRegression(solver='lbfgs',multi_class='multinomial',
+                              random_state=1)
+#    clf2 = RandomForestClassifier(n_estimators=50, random_state=1)
+#    clf3 = GaussianNB()
+    model = VotingClassifier(estimators=[('rg',clf1)])
+    model.fit(stackedX, yc_test)
     return model
 
 def Stacked_prediction(members,model,ob):
@@ -161,25 +180,33 @@ def Stacked_Ensemble(NUM):
     ## Preprocessing
     from keras.utils import to_categorical  
     X_raw_seq = [Preprocess(seg) for seg in X_raw]
-    y_raw_cat = to_categorical(y_raw).astype('int32') 
+    y_raw = to_categorical(y_raw).astype('int32') 
+    ## Create the Vocab
+    ## Read Observe Data
+    with open(OBSERVE_PATH, encoding = 'utf8') as f:
+        ob = f.readlines()
+    ob_id = [seg.strip().split(",",1)[0] for seg in ob][1:]
+    ob = [seg.strip().split(",",1)[1] for seg in ob][1:]    
+    tokenizer, word_index = Create_vocab(X_raw_seq + ob, save=True)
     ## Encoding  
-    X_raw, y_raw, word_index = Encoding(X_raw_seq,y_raw_cat) 
+    X_raw = Encoding(X_raw_seq,tokenizer) 
     ## Get Training set and Testing set
     from sklearn.model_selection import train_test_split   
     X_train,X_test,y_train,y_test = train_test_split(X_raw,y_raw,train_size=0.7,shuffle=True)    
     ### Training Parameters ###
-    VAL_SIZE = 0.2
+    VAL_SIZE = 0.3
     SEQUENCES_SIZE = np.shape(X_raw)[1] # columns dim size of sequences
     LEXION_SIZE = len(word_index)
-    HIDDEN_DIM = 32
+    HIDDEN_DIM = 256
     ###########################
     
     ## Using the Pre-trained Word-Vector
-#    EMB_SEQ_SIZE, EMB_INPUT_SIZE, embedding_matrix = PreTrain_weights(X_raw_seq, word_index, HIDDEN_DIM)
-#    X_raw, y_raw, word_index = Encoding(X_raw_seq,y_raw_cat,EMB_SEQ_SIZE)
-#    X_train,X_test,y_train,y_test = train_test_split(X_raw,y_raw,train_size=0.7,shuffle=True) 
+    EMB_SEQ_SIZE, EMB_INPUT_SIZE, embedding_matrix = PreTrain_weights(X_raw_seq, word_index, HIDDEN_DIM)
+    X_raw = Encoding(X_raw_seq,tokenizer,EMB_SEQ_SIZE)
+    X_train,X_test,y_train,y_test = train_test_split(X_raw,y_raw,train_size=0.7,shuffle=True) 
    
     ## Train and Save Sub-Models  
+    all_models = list()
     for i in range(NUM):
         # Get Training set and Validation set
         X,X_val,y,y_val = train_test_split(X_train,y_train,
@@ -187,42 +214,51 @@ def Stacked_Ensemble(NUM):
                                            random_state=i,
                                            shuffle=True)
         # Build new model
-        model = Build_model(SEQUENCES_SIZE,LEXION_SIZE+1,HIDDEN_DIM)
-#        model = Build_model(EMB_SEQ_SIZE,EMB_INPUT_SIZE,HIDDEN_DIM,embedding_matrix)
+#        model = Build_model(SEQUENCES_SIZE,LEXION_SIZE+1,HIDDEN_DIM)
+        model = Build_model(EMB_SEQ_SIZE,EMB_INPUT_SIZE,HIDDEN_DIM,embedding_matrix)
         # Train the model
         Train_model(X,y,X_val,y_val,model,i)
-        
+        all_models.append(model)
+    
     ## Separate Stacking Model
     from keras.models import load_model
-    all_models = list()
     for i in range(NUM):
         model = load_model(MODE_PATH + '/model_'+ str(i) +'.h5')
         all_models.append(model)   
     yc_test = np.argmax(y_test,axis=-1) # LogisticRegression's target values    
     model = Fit_stacked_model(all_models,X_test,yc_test)
-    ## Evaluate The Model
-    from sklearn.metrics import accuracy_score
-    y_pred = Stacked_prediction(all_models, model, X_test)
-    acc = accuracy_score(yc_test, y_pred)
-    print('Stacked Test Accuracy: %.3f' % acc)
+#    ## Evaluate The Model
+#    from sklearn.metrics import accuracy_score
+#    y_pred = Stacked_prediction(all_models, model, X_test)
+#    acc = accuracy_score(yc_test, y_pred)
+#    print('Stacked Test Accuracy: %.3f' % acc)
     
-    ## Read Observe Data
-    with open(OBSERVE_PATH, encoding = 'utf8') as f:
-        ob = f.readlines()
-    ob_id = [seg.strip().split(",",1)[0] for seg in ob][1:]
-    ob = [seg.strip().split(",",1)[1] for seg in ob][1:]
     ## Preprocessing
     ob_id = np.asarray(ob_id,dtype='int32')
-    ob = [Preprocess(seg) for seg in ob]    
+    ob = [Preprocess(seg) for seg in ob]  
+    ## Read the Vocab
+    import pickle
+    with open('tokenizer.pickle', 'rb') as f:
+        tokenizer = pickle.load(f)
     ## Encoding
-    ob, ob_id, word_index = Encoding(ob, ob_id, SEQUENCES_SIZE) 
-#    ob, ob_id, word_index = Encoding(ob, ob_id,EMB_SEQ_SIZE)
+#    ob = Encoding(ob, tokenizer, SEQUENCES_SIZE) 
+    ob = Encoding(ob, tokenizer, EMB_SEQ_SIZE)
     ## Submission
     print("Submission ...")
     pred = Stacked_prediction(all_models,model,ob)
     sampleSubmission = pd.read_csv(SUBMISSION_PATH)
-    sampleSubmission["label"] = np.argmax(pred,axis=-1)
+    sampleSubmission["label"] = pred
     sampleSubmission.to_csv(SUBMISSION_PATH,index=None)   
+
+def Plot_the_model(model):
+    ## SVG
+    from IPython.display import SVG
+    from keras.utils.vis_utils import model_to_dot  
+    model.summary()
+    return SVG(model_to_dot(model, show_shapes=True, show_layer_names=True, rankdir='HB').create(prog='dot', format='svg'))
 
 if __name__ == '__main__':
     Stacked_Ensemble(1)
+    from keras.models import load_model
+    model = load_model(MODE_PATH + '/model_'+ str(0) +'.h5')
+    Plot_the_model(model)
